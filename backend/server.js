692ac3605fed4058962ea99300a8fcc014508d8a
DEEP MODI <vishwmodi@gmail.com>
2025-08-02 17:10:47 +0530

    Yeap chat implemented

--- backend/server.js
+++ backend/server.js
@@ -13,16 +13,43 @@ const io = new Server(server, {
 
 app.use(express.json());
 
+const users = {};
+const All_Users = [];
+
 io.on("connection", (socket) => {
   console.log("User Connected", socket.id);
-  socket.emit("all-users", All_Users);
+
+  socket.on("register", (name) => {
+    users[name] = socket.id;
+    console.log(`User: ${name} registered with socket id:${socket.id} `);
+    io.emit(
+      "all-users",
+      Object.entries(users).map(([name, id]) => ({
+        name,
+        id,
+      }))
+    );
+
+    console.log(users);
+  });
+
+  socket.on("private-message", ({ to, message, from }) => {
+    const targetedSocketId = users[to];
+    if (targetedSocketId) {
+      io.to(targetedSocketId).emit("recieve-message", {
+        from,
+        message,
+      });
+    }
+  });
 });
 
-const All_Users = [];
 app.post("/register", (req, res) => {
   console.log("Body", req.body);
   All_Users.push(req.body);
   io.emit("new-user", req.body);
+  console.log(All_Users);
+
   res.send({ status: "Ok" });
 });
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,108 @@
 import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
+import "./App.css";
+
+const socket = io("http://localhost:3000");
 
 function App() {
-  const [user, setUser] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [toUser, setToUser] = useState([]);
+  const [username, setUsername] = useState("");
+  const [message, setMessage] = useState("");
+  const [chat, setChat] = useState([]);
 
   useEffect(() => {
-    const socket = io("http://localhost:3000");
-
     socket.on("all-users", (users) => {
-      setUser(users);
+      console.log(users);
+      setUsers(users);
     });
 
-    socket.on("new-user", (users) => {
-      setUser((prev) => [...prev, users]);
+    socket.on("recieve-message", ({ from, message }) => {
+      setChat((prev) => [...prev, { from, message }]);
     });
-    return () => socket.disconnect();
+    return () => {
+      socket.off("all-users");
+      socket.off("recieve-message");
+    };
   }, []);
+
+  const handleRegister = () => {
+    socket.emit("register", username);
+  };
+
+  const handleSend = () => {
+    try {
+      socket.emit("private-message", {
+        to: toUser,
+        message,
+        from: username,
+      });
+      setChat((prev) => [...prev, { from: "You", message }]);
+      setMessage("");
+    } catch (error) {
+      console.log("error while sending message", error);
+    }
+  };
+
   return (
     <div className="App">
       <h1>Users</h1>
 
       <ul>
-        {user.map((user, i) =>
-          user?.name ? <li key={i}>{user.name}</li> : null
-        )}
+        {users.map((user, i) => (
+          <li key={i}>{user.name}</li>
+        ))}
       </ul>
+
+      <div>
+        <h2>Private Chat</h2>
+        <div>
+          <input
+            type="text"
+            placeholder="Your name"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <button onClick={handleRegister}>Register Your Self</button>
+        </div>
+        <div>
+          <input
+            type="text"
+            placeholder="Your Message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <select onChange={(e) => setToUser(e.target.value)}>
+            <option value="">-- Select a user --</option>
+            {users.map((user) => (
+              <option key={user.name} value={user.name}>
+                {user.name}
+              </option>
+            ))}
+          </select>
+
+          <button onClick={handleSend}>Send</button>
+        </div>
+        <div>
+          <h2>Chat: </h2>
+          <div className="chat-container">
+            {chat.map((chat, i) =>
+              chat ? (
+                <div
+                  key={i}
+                  className={`message ${
+                    chat.from === "You" ? "sent" : "received"
+                  }`}
+                >
+                  <div className="meta">
+                    {chat.from === "You" ? "You" : chat.from}
+                  </div>
+                  <div className="text">{chat.message}</div>
+                </div>
+              ) : null
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
